7e86189f81c1de5d1a546e45b6dbdf01c031b7ca
FFelle7 <felixnorden07@gmail.com>
2025-05-27 18:53:08 +0200

    installation

--- src/routes/(scroll)/+page.svelte
+++ src/routes/(scroll)/+page.svelte
@@ -566,7 +566,7 @@ function handleScroll() {
 		overflow: hidden;
 		padding-top: 50px;
 		/*background: radial-gradient(circle, #202057, #04040d);*/
-		background-image: url("{base}/src/stores/images/background1.png");
+		background-image: url("{base}/images/background1.png");
 	}
 
 	@keyframes flip {
@@ -747,7 +747,7 @@ function handleScroll() {
 	/* LOGIN SECTION BACKGROUND */
 	.login-section {
 		/*background: content-box radial-gradient(rgba(32, 32, 87, 0.351), rgb(0, 0, 0));*/
-		background-image: url("{base}/src/stores/images/background4.png");
+		background-image: url("{base}/images/background4.png");
 	}
 
 	.login-form {
@@ -822,7 +822,7 @@ function handleScroll() {
 		justify-content: center;
 		text-align: center;
 		/*background: content-box radial-gradient(rgba(32, 32, 87, .5), rgb(0, 0, 0));*/
-		background-image: url("{base}/src/stores/images/background2.png");
+		background-image: url("{base}/images/background2.png");
 		color: white;
 	}
 
